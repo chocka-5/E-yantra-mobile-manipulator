197b3251483c2a0942e5e06d3a5786e8f520cb14
chocka-5 <chockalingam0412@gmail.com>
2019-12-09 20:35:27 +0530

    Add files via upload

---
+++ codes/xbee.ino
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+void usart_init(void);
+void usart_tx(unsigned char msg);
+
+
+int main(void)
+{usart_init();
+while(1){
+ 
+usart_tx('6');             //increment count after transmission of first character
+}}
+void usart_init()
+{
+ SREG|=0x80;
+ UCSR0A=0X00;                      //set global interrupt enable bit
+ UCSR0B=0X00; 
+ UBRR0H=0x00;
+ UBRR0L=0X5F;                      //baud rate 4800, % error 0.2 %
+ UCSR0B|=(1<<TXEN0)|(1<<RXEN0);   //enable transmitter, receiver, enable transmit complete interrupt
+ UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00);         //8-bit data, 1 stop bit, parity disabled, asynchronous operation
+}
+void usart_tx(unsigned char msg)
+{
+ while(!(UCSR0A & (UCSR0A|=1<<UDRE0)))               //wait until transmit buffer becomes empty 
+{UDR0=msg;
+ 
+ }//transmit first character by polling 
+}
+
+ 
+ 
---
+++ codes/xbee2.ino
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+void uart0_init(void)
+{
+ UCSR0B = 0x00; //disable while setting baud rate
+ UCSR0A = 0x00;
+ UCSR0C = 0x06;
+ UBRR0L = 0x5F; //set baud rate lo
+ UBRR0H = 0x00; //set baud rate hi
+ UCSR0B = 0xE8;
+}
+void init_devices()
+{
+cli(); //Clears the global interrupts
+
+uart0_init(); //Initialize UART1 for serial communication
+sei(); //Enables the global interrupts
+}
+int flag=0;
+SIGNAL (SIG_UART0_DATA)
+{flag=1;
+UDR0 = 'A';}
+SIGNAL (SIG_UART0_TRANS)
+{
+  flag=2;
+}
+
+int main()
+{
+
+  init_devices();
+  while(1);
+}
+
+
